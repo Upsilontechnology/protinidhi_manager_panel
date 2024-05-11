de0d9d2d9f1a781fc303b875493c6ad05489925c
akilinjamam <akilinjamam@gmail.com>
2024-05-11 08:15:32 +0600

    registration-ui-done

---
+++ src/components/registration/Registration.jsx
@@ -0,0 +1,40 @@
+import '../global_style/global_style.css';
+import reg from './Registration.module.scss';
+import logo from '../../icons/Layer_1.png';
+import { allInputs } from './allInput';
+import useRegistration from './useRegistration';
+const Registration = () => {
+    const  {open, setOpen} = useRegistration();
+    allInputs[2].type = open ? 'text' : 'password'
+    return (
+        <div className={reg.main}>
+            <div className={`${reg.top_bar} flex_center `}>
+                <img src={logo} alt="" />
+            </div>
+            <div className={`${reg.container}  flex_center`}>
+                <div className={`${reg.registration_part} flex_top`}>
+                    <div className={`${reg.registration_part_container} `}>
+                    {
+                        allInputs?.map((input, index) => {
+                            return (
+                                <input className={reg.input} key={index + 1} placeholder={input.placeHolder} type={input.type} />
+                            )
+                        })
+                    }
+                    <i onClick={() => setOpen(!open)} className={`${reg.eye} ${open ? 'uil uil-eye': 'uil uil-eye-slash'}`}></ i>
+                    <div className={`${reg.checkInput} flex_around`}>
+                        <input  type="checkbox" name="" id="" />
+                        <span>Remember Me</span>
+                    </div>
+
+                    <input className={`${reg.signIn}`} type="submit" value="Sign In" />
+
+                    <p className={`${reg.footer_title}`}>Already have an Account?<span>Log In</span></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Registration;
---
+++ src/components/registration/allInput.js
@@ -0,0 +1,17 @@
+export const allInputs = [
+    {
+        type: 'text',
+        value: 'name',
+        placeHolder: 'Name'
+    },
+    {
+        type: 'email',
+        value: 'email',
+        placeHolder: 'Your Email'
+    },
+    {
+        type: 'password',
+        value: 'password',
+        placeHolder: 'Your Password'
+    },
+]
