fe6c5add7396fb09bd36e3cc2912a5285171fdfa
akilinjamam <akilinjamam@gmail.com>
2024-05-13 11:30:05 +0600

    file-updated

---
+++ src/components/login/allLoginInput.js
@@ -0,0 +1,12 @@
+export const allLoginInputs = [
+    {
+        type: 'email',
+        value: 'email',
+        placeHolder: 'Your Email'
+    },
+    {
+        type: 'password',
+        value: 'password',
+        placeHolder: 'Your Password'
+    },
+]
